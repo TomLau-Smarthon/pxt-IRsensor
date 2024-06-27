a93c9f576b45beb0c6bca76dfba4768e867184de
TomLau-Smarthon <tom.lau.smarthon@gmail.com>
2024-06-27 15:02:13 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
+/**
+ * IR detector block
+ */
+//% weight=999 color=#3CB371 icon="" block="IR detector"
+namespace IRdetector {
 
+    //% blockId="smarthon_IR_detector"
+    //% block="Object detected at %pin"
+
+    export function IRdetection(pin: DigitalPin): boolean {
+        if (pins.digitalReadPin(pin) == 0)
+            return true;
+        else return false;
+    }
+}
